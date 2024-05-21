515cd74bc9b9593cc9304db7f94bfff5fd3aba8b
Kwasako <danburamayuba@gmail.com>
2024-05-21 03:35:00 +0100

    use the UseFetch hook to fetch data from an api and render it

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from 'react'
 
-const UseFetch = (url) => {
-    const[data,setData]=useState()
+ const UseFetch = (url) => {
+    const[data,setData]=useState();
     useEffect(()=>{
-        fetch(url).then(res => res.json())
-        .then(data=>setData(data))
-    }, [])
-    return data
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+  return [data]
 }
 
 export default UseFetch
